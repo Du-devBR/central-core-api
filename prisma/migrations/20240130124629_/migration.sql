1a008dab677b13d0ca760fa65c82f23932ad5a61
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-02-02 18:03:25 -0300

    feat: removido migrations database user sem password

--- prisma/migrations/20240130124629_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ALTER COLUMN "modified_at" DROP NOT NULL;
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,11 +1,16 @@
+import "dotenv/config";
 import { z } from "zod";
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "produtction"]).default("dev"),
   PORT: z.coerce.number().default(3333),
+  GITHUB_CLIENT_ID: z.coerce.string(),
+  GITHUB_CLIENT_SECRET: z.coerce.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
+
+// console.log(process.env);
 
 if (_env.success === false) {
   console.error("Invalid enviroment variables", _env.error.format());
