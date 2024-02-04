0da51422269c7555c72f6d147a3dbba544b72d92
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-02-04 20:00:00 -0300

    feat: configurado use-case register

--- src/use-cases/users/register.ts
+++ src/use-cases/users/register.ts
@@ -1,22 +1,38 @@
 import { UsersRespository } from "@/respositories/interfaces/users-repository";
+import { User } from "@prisma/client";
+import { hash } from "bcryptjs";
 
 interface RegisterUseCaseRequest {
   name: string;
   lastname: string;
   email: string;
-  github: string;
+  password: string;
+}
+
+interface RegisterUseCaseResponse {
+  user: User;
 }
 
 export class RegisterUseCase {
   // eslint-disable-next-line no-useless-constructor
   constructor(private usersRepository: UsersRespository) {}
 
-  async execute({ name, lastname, github, email }: RegisterUseCaseRequest) {
-    await this.usersRepository.create({
+  async execute({
+    name,
+    lastname,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const password_hash = await hash(password, 6);
+    const user = await this.usersRepository.create({
       name,
-      email,
-      github,
       lastname,
+      email,
+      password_hash,
     });
+
+    return {
+      user,
+    };
   }
 }
