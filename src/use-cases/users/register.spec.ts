ad73f4950495556962d89337ea004295cd5a8b98
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-02-07 20:43:18 -0300

    feat: criado teste para verificar integração entre vistest e o codigo

--- src/use-cases/users/register.spec.ts
+++ src/use-cases/users/register.spec.ts
@@ -2,6 +2,6 @@ import { describe, expect, it } from "vitest";
 
 describe("testing connection", () => {
   it("2 + 2 = 4", () => {
-    expect(2 + 2).toEqual(4);
+    expect(3 + 2).toEqual(5);
   });
 });
