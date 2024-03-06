7a490dd65926936ac22d5dfbb0f8adcc0b6d4dbb
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-03-06 20:40:44 -0300

    test: criado test para validar se busca por texto retorna um array

---
+++ src/use-cases/questions/get-bytext-typed.spec.ts
@@ -0,0 +1,33 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { InMemoryQuestionsRepository } from "@/respositories/in-memory/in-memory-questions-repository";
+import { GetByTextTypedQuestionUseCase } from "./get-by-text-typed";
+
+let questionRepository: InMemoryQuestionsRepository;
+let sut: GetByTextTypedQuestionUseCase;
+
+describe("Get question by text typed use case", () => {
+  beforeEach(() => {
+    questionRepository = new InMemoryQuestionsRepository();
+    sut = new GetByTextTypedQuestionUseCase(questionRepository);
+  });
+
+  it("should return question by given text typed", async () => {
+    await questionRepository.create({
+      title: "Question 1",
+      content: "Content 1",
+      user_id: "user1",
+    });
+
+    await questionRepository.create({
+      title: "Question 1",
+      content: "content 2",
+      user_id: "user1",
+    });
+
+    const inputText = "tion";
+
+    const result = await sut.execute({ text: inputText });
+
+    expect(result.question).toHaveLength(2);
+  });
+});
