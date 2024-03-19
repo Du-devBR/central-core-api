593bdcea9492408846d155b2fc2e00c1b2f6e288
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-03-19 18:27:33 -0300

    feat: criado use case para listar as answers filhas com base no questionId e parentID

--- src/respositories/interfaces/answers-repository-interface.ts
+++ src/respositories/interfaces/answers-repository-interface.ts
@@ -3,4 +3,8 @@ import { Answer, Prisma } from "@prisma/client";
 export interface AnswersRepository {
   create(data: Prisma.AnswerUncheckedCreateInput): Promise<Answer>;
   findAllAnswersParentInTheQuestion(questionId: string): Promise<Answer[]>;
+  findAllAnswersChildInTheParent(
+    questionId: string,
+    parentId: string,
+  ): Promise<Answer[]>;
 }
